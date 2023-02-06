dbd3ca570946bec6ff85d61b1dd447c71cd1d3f2
EC2 Default User <ec2-user@ip-172-31-36-110.ap-northeast-1.compute.internal>
2023-02-06 11:28:12 +0000

    [add]no-image画像の設定

--- app/models/shop.rb
+++ app/models/shop.rb
@@ -1,4 +1,13 @@
 class Shop < ApplicationRecord
   has_one_attached :shop_image
   belongs_to :customer
+
+  def get_shop_image(width, height)
+    unless shop_image.attached?
+      file_path = Rails.root.join('app/assets/images/noimage.jpg')
+      shop_image.attach(io: File.open(file_path), filename: 'default-image.jpg', content_type: 'image/jpeg')
+    end
+      shop_image.variant(resize_to_limit: [width, height]).processed
+  end
+
 end
