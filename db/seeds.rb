e5e43615a3f2d4d2205637d4c504e838ed974941
EC2 Default User <smsk329@gmail.com>
2023-03-23 11:00:55 +0000

    [fixed]管理者側ログインアドレス変更

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,6 +6,6 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 Admin.create!(
-  email: "admin@gmail.com",
+  email: "admin@example.com",
   password: "admin_test",
 )
